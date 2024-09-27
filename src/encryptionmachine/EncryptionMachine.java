09dca814b15583ca3b683e1245b752c65af14fdb
Lance Balstad <Lance.balstad@ndsu.edu>
2024-09-27 16:38:14 -0500

    first commit

---
+++ src/encryptionmachine/EncryptionMachine.java
@@ -0,0 +1,14 @@
+package encryptionmachine;
+
+/**
+ *
+ * @author Balst
+ */
+public class EncryptionMachine {
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("helo wrld");
+    }
+    
+}
